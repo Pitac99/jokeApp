3efbbf59e03a6ec08e034da954df7528e3590228
Cristi <cristi.david87@gmail.com>
2022-05-18 23:08:02 +0300

    update app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Joke from './components/Joke';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+          jokes: [],
+        }
+      }
+
+componentDidMount(){
+  fetch('https://api.chucknorris.io/jokes/random')
+  .then(response => response.json())
+   .then(info => this.setState({jokes:info}))
+   console.log('prima-incarcare')
+}
+
+render()
+{ const finalJoke = this.state.jokes.value;
+  const handleClick = () =>{
+    fetch('https://api.chucknorris.io/jokes/random')
+    .then(response => response.json())
+    .then(users => this.setState({jokes:users}))
+    console.log('click')
+  }
+return (
+  
+    <div>
+      <Joke randomJoke={finalJoke}/>
+      <button onClick={handleClick} className="garamond f4 br2 ph3 pv2 dib mb2 white bg-black b ml7 mt3">
+       Random Joke
+       </button>
     </div>
   );
-}
+}}
 
 export default App;
