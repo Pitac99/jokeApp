d7e53a72ac20d29a5d14ca6ce3339d10c4fa5bde
Cristi <cristi.david87@gmail.com>
2022-05-19 00:40:55 +0300

    Updates

---
+++ precache-manifest.4ce44d376e6a30811f1c3a9ada99060b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4cc637177a7fc6f1efd",
+    "url": "/static/css/main.e800de5c.chunk.css"
+  },
+  {
+    "revision": "f4cc637177a7fc6f1efd",
+    "url": "/static/js/main.f4cc6371.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "957e0c4624083ab359d2",
+    "url": "/static/css/2.3a920ac3.chunk.css"
+  },
+  {
+    "revision": "957e0c4624083ab359d2",
+    "url": "/static/js/2.957e0c46.chunk.js"
+  },
+  {
+    "revision": "82555cf690c0363f7ef9",
+    "url": "/static/js/runtime~main.82555cf6.js"
+  },
+  {
+    "revision": "836489ed5cb1b8b4206ec76cd685d064",
+    "url": "/static/media/Chuck_Norris.836489ed.otf"
+  },
+  {
+    "revision": "ae451b2bebd7c20e9ca4865c6151fb5b",
+    "url": "/static/media/background.ae451b2b.jpg"
+  },
+  {
+    "revision": "7a6e676a23fa6f82fd58fe91486b82f6",
+    "url": "/index.html"
+  }
+];
